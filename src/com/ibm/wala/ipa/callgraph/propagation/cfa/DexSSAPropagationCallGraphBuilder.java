274ba56706f6df77f0cdb4173afdf4855fe9cfc9
Steve Suh <suhsteve@live.com>
2012-08-06 15:34:42 -0700

    changed superclass of DexSSAPropagationCallGraphBuilder

--- src/com/ibm/wala/ipa/callgraph/propagation/cfa/DexSSAPropagationCallGraphBuilder.java
+++ src/com/ibm/wala/ipa/callgraph/propagation/cfa/DexSSAPropagationCallGraphBuilder.java
@@ -2,24 +2,27 @@ package com.ibm.wala.ipa.callgraph.propagation.cfa;
 
 import com.ibm.wala.ipa.callgraph.AnalysisCache;
 import com.ibm.wala.ipa.callgraph.AnalysisOptions;
-import com.ibm.wala.ipa.callgraph.CGNode;
 import com.ibm.wala.ipa.callgraph.ContextSelector;
 import com.ibm.wala.ipa.callgraph.impl.DexExplicitCallGraph;
 import com.ibm.wala.ipa.callgraph.impl.ExplicitCallGraph;
+import com.ibm.wala.ipa.callgraph.propagation.InstanceKeyFactory;
 import com.ibm.wala.ipa.callgraph.propagation.SSAContextInterpreter;
+import com.ibm.wala.ipa.callgraph.propagation.SSAPropagationCallGraphBuilder;
 import com.ibm.wala.ipa.cha.IClassHierarchy;
 
 public class DexSSAPropagationCallGraphBuilder extends
-	ZeroXCFABuilder {
+	SSAPropagationCallGraphBuilder {
 	
 
 	
 	public DexSSAPropagationCallGraphBuilder(IClassHierarchy cha,
 			AnalysisOptions options, AnalysisCache cache,
 			ContextSelector appContextSelector,
-			SSAContextInterpreter appContextInterpreter, int instancePolicy) {
-		super(cha, options, cache, appContextSelector, appContextInterpreter,
-				instancePolicy);
+			SSAContextInterpreter appContextInterpreter, InstanceKeyFactory instanceKeys) {
+	    super(cha, options, cache, new DefaultPointerKeyFactory());
+	    setContextSelector(appContextSelector);
+	    setContextInterpreter(appContextInterpreter);
+	    setInstanceKeys(instanceKeys);
 	}
 
 	@Override
