02a0a8d4679a01e1cebe598feffb5f21baf97432
Rogan Creswick <creswick@gmail.com>
2012-08-21 17:20:28 -0700

    added test framework and ant test target, with sample failing test

---
+++ test/util/IFDSTaintFlowFunctionProviderTest.java
@@ -0,0 +1,29 @@
+/**
+ * IFDSTaintFlowFunctionProviderTest.java
+ * 
+ * Copyright (c) 2012 Galois, Inc.
+ */
+package util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.ibm.wala.ssa.ISSABasicBlock;
+
+/**
+ * @author creswick
+ *
+ */
+public class IFDSTaintFlowFunctionProviderTest {
+
+    @Test
+    public final void test() {
+        IFDSTaintFlowFunctionProvider<ISSABasicBlock> fnProvider =
+                new IFDSTaintFlowFunctionProvider<ISSABasicBlock>(null, null, null);
+        
+        fnProvider.getNormalFlowFunction(null, null);
+        fail("not yet implemented");
+    }
+
+}
