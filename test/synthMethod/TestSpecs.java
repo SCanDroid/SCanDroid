400d846d2bee78e437d5455c6073d4dacc84447b
Rogan Creswick <creswick@gmail.com>
2012-09-12 11:17:36 -0700

    removed some obsolete code from TestSpecs

--- test/synthMethod/TestSpecs.java
+++ test/synthMethod/TestSpecs.java
@@ -8,39 +8,6 @@ import spec.SinkSpec;
 import spec.SourceSpec;
 
 public class TestSpecs implements ISpecs {
-	/*
-	private SourceSpec[] sources;
-	private SinkSpec[] sinks;
-	
-
-	public TestSpecs(Collection<CGNode> nodes) {
-	    
-	    // Just create EntryArgSourceSpecs for a specific method,
-	    // and CallArgSourceSpecs for a different method.
-	    // Those should be sufficient to establish a data flow we can use for 
-	    /// testing.
-	    
-		ArrayList<SourceSpec> sourcesAL = new ArrayList<SourceSpec>();
-		ArrayList<SinkSpec> sinksAL = new ArrayList<SinkSpec>();
-
-        for (CGNode node:nodes) {
-        	String nodeClass = node.getMethod().getDeclaringClass().getName().toString();
-        	String nodeMethod = node.getMethod().getName().toString();
-        	MethodNamePattern mnp = new MethodNamePattern(nodeClass, nodeMethod);
-        	FieldNamePattern fnp = new FieldNamePattern(nodeClass, nodeMethod);
-        	
-        	sourcesAL.add(new EntryArgSourceSpec(mnp, new int[] {}));
-        	sourcesAL.add(new FieldSourceSpec(fnp));
-        	sinksAL.add(new EntryRetSinkSpec(mnp));
-        	sinksAL.add(new FieldSinkSpec(fnp));
-
-        }        
-        sources = sourcesAL.toArray(new SourceSpec[sourcesAL.size()]);        
-        sinks = sinksAL.toArray(new SinkSpec[sinksAL.size()]);
-
-	}
-	*/
-
     @Override
     public MethodNamePattern[] getEntrypointSpecs() {
         return null;
