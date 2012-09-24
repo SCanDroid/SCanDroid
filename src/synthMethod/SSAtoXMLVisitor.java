0322651f0e8f3395222c4bb5f83c3de7277ba491
Rogan Creswick <creswick@gmail.com>
2012-09-24 09:44:57 -0700

    added visitGet and visitPut implementations; changed tabs to spaces

--- src/synthMethod/SSAtoXMLVisitor.java
+++ src/synthMethod/SSAtoXMLVisitor.java
@@ -1,5 +1,6 @@
 package synthMethod;
 
+import java.io.UTFDataFormatException;
 import java.util.List;
 import java.util.Map;
 
@@ -33,218 +34,336 @@ import com.ibm.wala.ssa.SSASwitchInstruction;
 import com.ibm.wala.ssa.SSAThrowInstruction;
 import com.ibm.wala.ssa.SSAUnaryOpInstruction;
 import com.ibm.wala.types.TypeReference;
+import com.ibm.wala.util.strings.Atom;
 
 public class SSAtoXMLVisitor implements SSAInstruction.IVisitor {
 
-	/**
-	 * A counter to use for generating unique local definition names.
-	 */
-	private int defCounter = 0;
+    /**
+     * A counter to use for generating unique local definition names.
+     */
+    private int defCounter = 0;
 
-	/**
-	 * Map the known defNum inst to local def names.
-	 */
-	private Map<Integer, String> localDefs = Maps.newHashMap();
+    /**
+     * Map the known defNum to local def names.
+     */
+    private Map<Integer, String> localDefs = Maps.newHashMap();
 
-	/** 
-	 * XML document to use for creating elements.
-	 */
-	private final Document doc;
-	
-	/**
-	 * XML elements that represent the ssa instructions
-	 */
-	private final List<Element> summary = Lists.newArrayList();
-	
-	public SSAtoXMLVisitor(Document doc) {
-		this.doc = doc;
-	}
-	
-	@Override
-	public void visitGoto(SSAGotoInstruction instruction) {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * XML document to use for creating elements.
+     */
+    private final Document doc;
 
-	@Override
-	public void visitArrayLoad(SSAArrayLoadInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * XML elements that represent the ssa instructions
+     */
+    private final List<Element> summary = Lists.newArrayList();
 
-	@Override
-	public void visitArrayStore(SSAArrayStoreInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    public SSAtoXMLVisitor(Document doc) {
+        this.doc = doc;
+    }
 
-	@Override
-	public void visitBinaryOp(SSABinaryOpInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void visitGoto(SSAGotoInstruction instruction) {
+        // TODO Auto-generated method stub
+    }
 
-	@Override
-	public void visitUnaryOp(SSAUnaryOpInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void visitArrayLoad(SSAArrayLoadInstruction instruction) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void visitConversion(SSAConversionInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void visitComparison(SSAComparisonInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void visitArrayStore(SSAArrayStoreInstruction instruction) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void visitConditionalBranch(
-			SSAConditionalBranchInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void visitSwitch(SSASwitchInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void visitBinaryOp(SSABinaryOpInstruction instruction) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void visitReturn(SSAReturnInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void visitGet(SSAGetInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void visitUnaryOp(SSAUnaryOpInstruction instruction) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void visitPut(SSAPutInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void visitInvoke(SSAInvokeInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void visitConversion(SSAConversionInstruction instruction) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void visitNew(SSANewInstruction instruction) {
-		try {
-			int defNum = instruction.getDef();
-			String localName = newLocalDef(defNum);
+    }
 
-			TypeReference type = instruction.getConcreteType();
-			String className = type.getName().getClassName().toUnicodeString();
-			
-			Element elt = doc.createElement(XMLSummaryWriter.E_NEW);
-			elt.setAttribute(XMLSummaryWriter.A_DEF, localName);
-			elt.setAttribute(XMLSummaryWriter.A_CLASS, className);
-			summary.add(elt);
-		} catch (Exception e) {
-			throw new SSASerializationException(e);
-		}
-	}
+    @Override
+    public void visitComparison(SSAComparisonInstruction instruction) {
+        // TODO Auto-generated method stub
 
+    }
 
-	@Override
-	public void visitArrayLength(SSAArrayLengthInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void visitConditionalBranch(
+            SSAConditionalBranchInstruction instruction) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void visitThrow(SSAThrowInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void visitMonitor(SSAMonitorInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void visitSwitch(SSASwitchInstruction instruction) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void visitCheckCast(SSACheckCastInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void visitInstanceof(SSAInstanceofInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void visitReturn(SSAReturnInstruction instruction) {
+        try {
+            String localName = getLocalName(instruction.getDef());
 
-	@Override
-	public void visitPhi(SSAPhiInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+            Element elt = doc.createElement(XMLSummaryWriter.E_RETURN);
+            elt.setAttribute(XMLSummaryWriter.A_VALUE, localName);
+            summary.add(elt);
+        } catch (Exception e) {
+            throw new SSASerializationException(e);
+        }
+    }
 
-	@Override
-	public void visitPi(SSAPiInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * eg:
+     * 
+     * <getfield class="Ljava/lang/Thread" field="runnable"
+     * fieldType="Ljava/lang/Runnable" def="x" ref="arg0" />
+     * 
+     * I think the get statics look like this:
+     * 
+     * <getstatic class="Ljava/lang/Thread" field="runnable"
+     * fieldType="Ljava/lang/Runnable" def="x" />
+     */
+    @Override
+    public void visitGet(SSAGetInstruction instruction) {
+        try {
+            String eltName;
 
-	@Override
-	public void visitGetCaughtException(
-			SSAGetCaughtExceptionInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+            // TODO I suspect this is the wrong boolean to check:
+            if (instruction.isStatic()) {
+                eltName = XMLSummaryWriter.E_GETSTATIC;
+            } else {
+                eltName = XMLSummaryWriter.E_GETFIELD;
+            }
+            Element elt = doc.createElement(eltName);
 
-	@Override
-	public void visitLoadMetadata(SSALoadMetadataInstruction instruction) {
-		// TODO Auto-generated method stub
-		
-	}
+            // TODO see above... same question about isStatic()
+            if (!instruction.isStatic()) {
+                String refName = getRefName(instruction.getRef());
+                elt.setAttribute(XMLSummaryWriter.A_REF, refName);
+            }
 
-	/**
-	 * Add a new defNum, creating a name for that defnum.
-	 * 
-	 * @param defNum
-	 */
-	private String newLocalDef(int defNum) {
-		String newName = "localdef_"+defCounter;
-		localDefs.put(defNum, newName);
-		defCounter++;
-		
-		return newName;
-	}
+            String def = getLocalName(instruction.getDef());
+            TypeReference fieldType = instruction.getDeclaredFieldType();
+            TypeReference classType = instruction.getDeclaredField()
+                    .getDeclaringClass();
 
-	/**
-	 * Get a local name for the provided defNum.
-	 * 
-	 * If, for some reason, the defNum has not yet been seen (and, thus, has no
-	 * local name associated with it) then this will throw an illegal state 
-	 * exception.
-	 * 
-	 * @param defNum
-	 * @return
-	 * @throws IllegalStateException
-	 */
-	private String getLocalName(int defNum) throws IllegalStateException {
-		if (localDefs.containsKey(defNum)) {
-			return localDefs.get(defNum);
-		}
-		throw new IllegalStateException("defNum: "+defNum+" is not defined.");
-	}
-	
-	public List<Element> getInstSummary() {
-		return summary;
-	}
+            String fieldName = instruction.getDeclaredField().getName()
+                    .toUnicodeString();
+
+            elt.setAttribute(XMLSummaryWriter.A_CLASS, typeRefToStr(classType));
+            elt.setAttribute(XMLSummaryWriter.A_FIELD, fieldName);
+            elt.setAttribute(XMLSummaryWriter.A_FIELD_TYPE,
+                    typeRefToStr(fieldType));
+            elt.setAttribute(XMLSummaryWriter.A_DEF, def);
+
+            summary.add(elt);
+        } catch (Exception e) {
+            throw new SSASerializationException(e);
+        }
+    }
+
+    /**
+     * <putstatic class="Ljava/lang/System" field="security"
+     * fieldType="Ljava/lang/SecurityManager" value="secure" />
+     * 
+     * <putfield class="Ljava/lang/Thread" field="runnable"
+     * fieldType="Ljava/lang/Runnable" ref="arg0" value="arg0" />
+     * 
+     */
+    @Override
+    public void visitPut(SSAPutInstruction instruction) {
+        try {
+            String eltName;
+
+            // TODO I suspect this is the wrong boolean to check:
+            if (instruction.isStatic()) {
+                eltName = XMLSummaryWriter.E_PUTSTATIC;
+            } else {
+                eltName = XMLSummaryWriter.E_PUTFIELD;
+            }
+            Element elt = doc.createElement(eltName);
+
+            // TODO see above... same question about isStatic()
+            if (!instruction.isStatic()) {
+                String refName = getRefName(instruction.getRef());
+                elt.setAttribute(XMLSummaryWriter.A_REF, refName);
+            }
+
+            String value = getLocalName(instruction.getVal());
+            TypeReference fieldType = instruction.getDeclaredFieldType();
+            TypeReference classType = instruction.getDeclaredField()
+                    .getDeclaringClass();
+
+            String fieldName = instruction.getDeclaredField().getName()
+                    .toUnicodeString();
+
+            elt.setAttribute(XMLSummaryWriter.A_CLASS, typeRefToStr(classType));
+            elt.setAttribute(XMLSummaryWriter.A_FIELD, fieldName);
+            elt.setAttribute(XMLSummaryWriter.A_FIELD_TYPE,
+                    typeRefToStr(fieldType));
+            elt.setAttribute(XMLSummaryWriter.A_VALUE, value);
+
+            summary.add(elt);
+        } catch (Exception e) {
+            throw new SSASerializationException(e);
+        }
+
+    }
+
+    @Override
+    public void visitInvoke(SSAInvokeInstruction instruction) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visitNew(SSANewInstruction instruction) {
+        try {
+            int defNum = instruction.getDef();
+            String localName = newLocalDef(defNum);
+
+            TypeReference type = instruction.getConcreteType();
+            String className = type.getName().getClassName().toUnicodeString();
+
+            Element elt = doc.createElement(XMLSummaryWriter.E_NEW);
+            elt.setAttribute(XMLSummaryWriter.A_DEF, localName);
+            elt.setAttribute(XMLSummaryWriter.A_CLASS, className);
+            summary.add(elt);
+        } catch (Exception e) {
+            throw new SSASerializationException(e);
+        }
+    }
+
+    @Override
+    public void visitArrayLength(SSAArrayLengthInstruction instruction) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visitThrow(SSAThrowInstruction instruction) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visitMonitor(SSAMonitorInstruction instruction) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visitCheckCast(SSACheckCastInstruction instruction) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visitInstanceof(SSAInstanceofInstruction instruction) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visitPhi(SSAPhiInstruction instruction) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visitPi(SSAPiInstruction instruction) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visitGetCaughtException(
+            SSAGetCaughtExceptionInstruction instruction) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visitLoadMetadata(SSALoadMetadataInstruction instruction) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * Add a new defNum, creating a name for that defnum.
+     * 
+     * @param defNum
+     */
+    private String newLocalDef(int defNum) {
+        String newName = "localdef_" + defCounter;
+        localDefs.put(defNum, newName);
+        defCounter++;
+
+        return newName;
+    }
+
+    /**
+     * Get a local name for the provided defNum.
+     * 
+     * If, for some reason, the defNum has not yet been seen (and, thus, has no
+     * local name associated with it) then this will throw an illegal state
+     * exception.
+     * 
+     * @param defNum
+     * @return
+     * @throws IllegalStateException
+     */
+    private String getLocalName(int defNum) throws IllegalStateException {
+        if (localDefs.containsKey(defNum)) {
+            return localDefs.get(defNum);
+        }
+        throw new IllegalStateException("defNum: " + defNum
+                + " is not defined.");
+    }
+
+    /**
+     * Calculate the correct ref name based on the incomming number.
+     * 
+     * Note that this needs to be consistent with XMLMethodSummaryReader's use
+     * of the symbolTable in that class.
+     * 
+     * @param ref
+     * @return
+     */
+    private String getRefName(int ref) {
+        if (0 == ref) {
+            return "unknown";
+        } else {
+            return "arg" + (ref - 1);
+        }
+    }
+
+    public List<Element> getInstSummary() {
+        return summary;
+    }
+
+    private String typeRefToStr(TypeReference fieldType)
+            throws UTFDataFormatException {
+        Atom className = fieldType.getName().getClassName();
+        Atom pkgName = fieldType.getName().getPackage();
+        return pkgName.toUnicodeString() + "/" + className.toUnicodeString();
+    }
 
 }
--- src/synthMethod/XMLSummaryWriter.java
+++ src/synthMethod/XMLSummaryWriter.java
@@ -17,157 +17,159 @@ import com.ibm.wala.types.TypeReference;
 import com.ibm.wala.util.strings.Atom;
 
 public class XMLSummaryWriter {
-	
-	//
-	// Define XML element names
-	//
+
+    //
+    // Define XML element names
+    //
     final static String E_CLASSLOADER = "classloader";
-	final static String E_METHOD = "method";
-	final static String E_CLASS = "class";
-	final static String E_PACKAGE = "package";
-	final static String E_CALL = "call";
-	final static String E_NEW = "new";
-	final static String E_POISON = "poison";
-	final static String E_SUMMARY_SPEC = "summary-spec";
-	final static String E_RETURN = "return";
-	final static String E_PUTSTATIC = "putstatic";
-	final static String E_GETSTATIC = "getstatic";
-	final static String E_AASTORE = "aastore";
-	final static String E_PUTFIELD = "putfield";
-	final static String E_GETFIELD = "getfield";
-	final static String E_ATHROW = "throw";
-	final static String E_CONSTANT = "constant";
+    final static String E_METHOD = "method";
+    final static String E_CLASS = "class";
+    final static String E_PACKAGE = "package";
+    final static String E_CALL = "call";
+    final static String E_NEW = "new";
+    final static String E_POISON = "poison";
+    final static String E_SUMMARY_SPEC = "summary-spec";
+    final static String E_RETURN = "return";
+    final static String E_PUTSTATIC = "putstatic";
+    final static String E_GETSTATIC = "getstatic";
+    final static String E_AASTORE = "aastore";
+    final static String E_PUTFIELD = "putfield";
+    final static String E_GETFIELD = "getfield";
+    final static String E_ATHROW = "throw";
+    final static String E_CONSTANT = "constant";
 
-	//
-	// Define XML attribute names
-	//
-	final static String A_NAME = "name";
-	final static String A_TYPE = "type";
-	final static String A_CLASS = "class";
-	final static String A_SIZE = "size";
-	final static String A_DESCRIPTOR = "descriptor";
-	final static String A_REASON = "reason";
-	final static String A_LEVEL = "level";
-	final static String A_WILDCARD = "*";
-	final static String A_DEF = "def";
-	final static String A_STATIC = "static";
-	final static String A_VALUE = "value";
-	final static String A_FIELD = "field";
-	final static String A_FIELD_TYPE = "fieldType";
-	final static String A_ARG = "arg";
-	final static String A_ALLOCATABLE = "allocatable";
-	final static String A_REF = "ref";
-	final static String A_INDEX = "index";
-	final static String A_IGNORE = "ignore";
-	final static String A_FACTORY = "factory";
-	final static String A_NUM_ARGS = "numArgs";
-	final static String V_NULL = "null";
-	final static String V_TRUE = "true";
-	
-	private final Document doc;
+    //
+    // Define XML attribute names
+    //
+    final static String A_NAME = "name";
+    final static String A_TYPE = "type";
+    final static String A_CLASS = "class";
+    final static String A_SIZE = "size";
+    final static String A_DESCRIPTOR = "descriptor";
+    final static String A_REASON = "reason";
+    final static String A_LEVEL = "level";
+    final static String A_WILDCARD = "*";
+    final static String A_DEF = "def";
+    final static String A_STATIC = "static";
+    final static String A_VALUE = "value";
+    final static String A_FIELD = "field";
+    final static String A_FIELD_TYPE = "fieldType";
+    final static String A_ARG = "arg";
+    final static String A_ALLOCATABLE = "allocatable";
+    final static String A_REF = "ref";
+    final static String A_INDEX = "index";
+    final static String A_IGNORE = "ignore";
+    final static String A_FACTORY = "factory";
+    final static String A_NUM_ARGS = "numArgs";
+    final static String V_NULL = "null";
+    final static String V_TRUE = "true";
 
-	public XMLSummaryWriter() throws ParserConfigurationException {
-        DocumentBuilderFactory docFactory =
-                DocumentBuilderFactory.newInstance();
+    private final Document doc;
+
+    public XMLSummaryWriter() throws ParserConfigurationException {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory
+                .newInstance();
         DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
         doc = docBuilder.newDocument();
         Element rootElement = doc.createElement(E_SUMMARY_SPEC);
         doc.appendChild(rootElement);
-	}
-	
-	/**
-	 * Throws various exceptions if a problem occurred serializing this method. 
-	 * 
-	 * No guarantees as to the state of the Document if an exception is thrown.
-	 * 
-	 * @param summary
-	 * @return
-	 * @throws DOMException
-	 * @throws UTFDataFormatException
-	 * @throws SSASerializationException
-	 */
-	public String serialize(MethodSummary summary) throws DOMException, UTFDataFormatException, SSASerializationException {
-		TypeReference methClass = summary.getMethod().getDeclaringClass();
-		Atom pkg = methClass.getName().getPackage();
-		Atom className = methClass.getName().getClassName();
-		Atom methodName = summary.getMethod().getName();
-		
-		// get an element to add this method to:
-		Element classElt = findOrCreateClassElt(pkg, className);
+    }
 
-		// create a method element, and populate it's attributes:
-		Element methElt = doc.createElement(E_METHOD);
-		methElt.setAttribute(A_NAME, methodName.toUnicodeString());
+    /**
+     * Throws various exceptions if a problem occurred serializing this method.
+     * 
+     * No guarantees as to the state of the Document if an exception is thrown.
+     * 
+     * @param summary
+     * @return
+     * @throws DOMException
+     * @throws UTFDataFormatException
+     * @throws SSASerializationException
+     */
+    public String serialize(MethodSummary summary) throws DOMException,
+            UTFDataFormatException, SSASerializationException {
+        TypeReference methClass = summary.getMethod().getDeclaringClass();
+        Atom pkg = methClass.getName().getPackage();
+        Atom className = methClass.getName().getClassName();
+        Atom methodName = summary.getMethod().getName();
 
-		String descriptor = getMethodDescriptor(summary);
-		methElt.setAttribute(A_DESCRIPTOR, descriptor);
+        // get an element to add this method to:
+        Element classElt = findOrCreateClassElt(pkg, className);
 
-		// default is false:
-		if (summary.isStatic()) {
-			methElt.setAttribute(A_STATIC, "true");
-		}
-		
-		// default is false:
-		if (summary.isFactory()) {
-			methElt.setAttribute(A_FACTORY, "true");
-		}
-		
-		// summarize the instructions:
-		List<Element> instructions = summarizeInstructions(summary);
-		for (Element elt : instructions) {
-			methElt.appendChild(elt);
-		}
-		
-		classElt.appendChild(methElt);
-		
-		return doc.getTextContent();
-	}
+        // create a method element, and populate it's attributes:
+        Element methElt = doc.createElement(E_METHOD);
+        methElt.setAttribute(A_NAME, methodName.toUnicodeString());
 
-	private Element findOrCreateClassElt(Atom pkg, Atom className)
-			throws UTFDataFormatException {
-		// TODO always creates a new element, needs to look-up if one exists
-		Element pkgElt = findOrCreatePkgElt(pkg);
-		Element classElt = doc.createElement(E_CLASS);
-		classElt.setAttribute(A_NAME, className.toUnicodeString());
-		pkgElt.appendChild(classElt);
-		return classElt;
-	}
+        String descriptor = getMethodDescriptor(summary);
+        methElt.setAttribute(A_DESCRIPTOR, descriptor);
 
-	private Element findOrCreatePkgElt(Atom pkg)
-			throws UTFDataFormatException {
-		// TODO always creates a new element, needs to look-up if one exists
-		Element pkgElt = doc.createElement(E_PACKAGE);
-		pkgElt.setAttribute(A_NAME, pkg.toUnicodeString());
-		doc.appendChild(pkgElt);
-		return pkgElt;
-	}
+        // default is false:
+        if (summary.isStatic()) {
+            methElt.setAttribute(A_STATIC, "true");
+        }
 
-	private List<Element> summarizeInstructions(MethodSummary summary) {
-		SSAtoXMLVisitor v = new SSAtoXMLVisitor(doc);		
+        // default is false:
+        if (summary.isFactory()) {
+            methElt.setAttribute(A_FACTORY, "true");
+        }
 
-		for ( SSAInstruction inst : summary.getStatements() ) {
-			inst.visit(v);
-		}
-		
-		return v.getInstSummary();
-	}
+        // summarize the instructions:
+        List<Element> instructions = summarizeInstructions(summary);
+        for (Element elt : instructions) {
+            methElt.appendChild(elt);
+        }
 
-	/**
-	 * Generate a method descriptor, such as (I[Ljava/lang/String;)[Ljava/lang/String
-	 * @param summary
-	 * @return
-	 */
-	private String getMethodDescriptor(MethodSummary summary) {
-		StringBuilder typeSigs = new StringBuilder("(");
-		for (int i=0; i < summary.getNumberOfParameters(); i++) {
-			TypeReference tr = summary.getParameterType(i);
-			
-			typeSigs.append(tr.getName().toUnicodeString());
-		}
-		typeSigs.append(")");
-		typeSigs.append(summary.getReturnType().getName().toUnicodeString());
-		String descriptor = typeSigs.toString();
-		return descriptor;
-	}
+        classElt.appendChild(methElt);
+
+        return doc.getTextContent();
+    }
+
+    private Element findOrCreateClassElt(Atom pkg, Atom className)
+            throws UTFDataFormatException {
+        // TODO always creates a new element, needs to look-up if one exists
+        Element pkgElt = findOrCreatePkgElt(pkg);
+        Element classElt = doc.createElement(E_CLASS);
+        classElt.setAttribute(A_NAME, className.toUnicodeString());
+        pkgElt.appendChild(classElt);
+        return classElt;
+    }
+
+    private Element findOrCreatePkgElt(Atom pkg) throws UTFDataFormatException {
+        // TODO always creates a new element, needs to look-up if one exists
+        Element pkgElt = doc.createElement(E_PACKAGE);
+        pkgElt.setAttribute(A_NAME, pkg.toUnicodeString());
+        doc.appendChild(pkgElt);
+        return pkgElt;
+    }
+
+    private List<Element> summarizeInstructions(MethodSummary summary) {
+        SSAtoXMLVisitor v = new SSAtoXMLVisitor(doc);
+
+        for (SSAInstruction inst : summary.getStatements()) {
+            inst.visit(v);
+        }
+
+        return v.getInstSummary();
+    }
+
+    /**
+     * Generate a method descriptor, such as
+     * (I[Ljava/lang/String;)[Ljava/lang/String
+     * 
+     * @param summary
+     * @return
+     */
+    private String getMethodDescriptor(MethodSummary summary) {
+        StringBuilder typeSigs = new StringBuilder("(");
+        for (int i = 0; i < summary.getNumberOfParameters(); i++) {
+            TypeReference tr = summary.getParameterType(i);
+
+            typeSigs.append(tr.getName().toUnicodeString());
+        }
+        typeSigs.append(")");
+        typeSigs.append(summary.getReturnType().getName().toUnicodeString());
+        String descriptor = typeSigs.toString();
+        return descriptor;
+    }
 }
