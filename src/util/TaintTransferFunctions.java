ec8ff026d798adf32626d885c10deb8218cf745f
Aaron Tomb <atomb@galois.com>
2012-10-24 15:32:33 -0700

    Begin to implement taint transfer function.

--- src/util/TaintTransferFunctions.java
+++ src/util/TaintTransferFunctions.java
@@ -1,5 +1,10 @@
 package util;
 
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.ibm.wala.dataflow.IFDS.IFlowFunction;
 import com.ibm.wala.dataflow.IFDS.IFlowFunctionMap;
 import com.ibm.wala.dataflow.IFDS.ISupergraph;
@@ -8,7 +13,10 @@ import com.ibm.wala.ipa.callgraph.CGNode;
 import com.ibm.wala.ipa.callgraph.propagation.PointerAnalysis;
 import com.ibm.wala.ipa.cfg.BasicBlockInContext;
 import com.ibm.wala.ssa.ISSABasicBlock;
+import com.ibm.wala.util.intset.IntSet;
+import com.ibm.wala.util.intset.MutableSparseIntSet;
 
+import domain.CodeElement;
 import domain.IFDSTaintDomain;
 
 public class TaintTransferFunctions <E extends ISSABasicBlock> implements
@@ -20,44 +28,94 @@ public class TaintTransferFunctions <E extends ISSABasicBlock> implements
 
     public TaintTransferFunctions(IFDSTaintDomain<E> domain,
             ISupergraph<BasicBlockInContext<E>, CGNode> graph, 
-            PointerAnalysis pa)
-    {
+            PointerAnalysis pa) {
         this.domain = domain;
         this.graph = graph;
         this.pa = pa;
     }
 
+    private final static class UseDefPair
+    {
+        private final CodeElement use;
+        private final CodeElement def;
+        public UseDefPair(CodeElement use, CodeElement def) {
+            this.use = use;
+            this.def = def;
+        }
+        public CodeElement getUse() {
+            return use;
+        }
+        public CodeElement getDef() {
+            return def;
+        }
+    }
+
+    private final class DefUse implements IUnaryFlowFunction {
+        private final List<UseDefPair> useToDefList = Lists.newArrayList();
+
+        private final BasicBlockInContext<E> bb;
+
+        public DefUse(final BasicBlockInContext<E> inBlock) {    
+            this.bb = inBlock;
+        }
+        
+        @Override
+        public IntSet getTargets(int d) {
+            MutableSparseIntSet set = MutableSparseIntSet.makeEmpty();
+            if(d == 0) {
+                /* Here we list what facts we add. All taints affecting uses of
+                 * this instruction also affect its definitions.
+                 */
+                set.add(0);
+            } else {
+                /* Here we list what facts we pass through. If a fact was true
+                 * before executing this instruction, it'll be true after,
+                 * unless we created a new definition of its associated
+                 * CodeElement.
+                 */
+            }
+            return set;
+        }
+    }
+
     @Override
-    public IUnaryFlowFunction getCallFlowFunction(BasicBlockInContext<E> arg0,
-            BasicBlockInContext<E> arg1, BasicBlockInContext<E> arg2) {
+    public IUnaryFlowFunction getCallFlowFunction(
+            BasicBlockInContext<E> src,
+            BasicBlockInContext<E> dest,
+            BasicBlockInContext<E> ret) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public IUnaryFlowFunction getCallNoneToReturnFlowFunction(
-            BasicBlockInContext<E> arg0, BasicBlockInContext<E> arg1) {
+            BasicBlockInContext<E> src,
+            BasicBlockInContext<E> dest) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public IUnaryFlowFunction getCallToReturnFlowFunction(
-            BasicBlockInContext<E> arg0, BasicBlockInContext<E> arg1) {
+            BasicBlockInContext<E> src,
+            BasicBlockInContext<E> dest) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public IUnaryFlowFunction getNormalFlowFunction(
-            BasicBlockInContext<E> arg0, BasicBlockInContext<E> arg1) {
+            BasicBlockInContext<E> src,
+            BasicBlockInContext<E> dest) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public IFlowFunction getReturnFlowFunction(BasicBlockInContext<E> arg0,
-            BasicBlockInContext<E> arg1, BasicBlockInContext<E> arg2) {
+    public IFlowFunction getReturnFlowFunction(
+            BasicBlockInContext<E> call,
+            BasicBlockInContext<E> src,
+            BasicBlockInContext<E> dest) {
         // TODO Auto-generated method stub
         return null;
     }
