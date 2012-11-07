831eb0ac5348778221ca87e99ead65b5497e0eef
Adam C. Foltzer <acfoltzer@galois.com>
2012-11-06 16:41:21 -0800

    code to map all permissions in a PScout result to method descriptors

--- test/org/scandroid/permissions/PScoutReaderTest.java
+++ test/org/scandroid/permissions/PScoutReaderTest.java
@@ -10,13 +10,21 @@ public class PScoutReaderTest {
 	public void getAccountsByFeatures() throws Throwable {
 		final String pscoutString = "<android.accounts.IAccountManager$Stub$Proxy: void getAccountsByFeatures(android.accounts.IAccountManagerResponse,java.lang.String,java.lang.String[])> ()";
 		final String expectedDescriptor = "android/accounts/IAccountManager$Stub$Proxy.getAccountsByFeatures(Landroid/accounts/IAccountManagerResponse;Ljava/lang/String;[Ljava/lang/String;)V";
-		assertTrue(PScoutReader.pscout2descriptor(pscoutString).equals(expectedDescriptor));
+		assertTrue(PScoutReader.pscout2descriptor(pscoutString).equals(
+				expectedDescriptor));
 	}
-	
+
 	@Test
-	public void setMasterSyncAutomatically () throws Throwable {
+	public void setMasterSyncAutomatically() throws Throwable {
 		final String pscoutString = "<android.content.ContentResolver: void setMasterSyncAutomatically(boolean)> (5)";
 		final String expectedDescriptor = "android/content/ContentResolver.setMasterSyncAutomatically(Z)V";
-		assertTrue(PScoutReader.pscout2descriptor(pscoutString).equals(expectedDescriptor));
+		assertTrue(PScoutReader.pscout2descriptor(pscoutString).equals(
+				expectedDescriptor));
+	}
+
+	@Test
+	public void generateMapping() throws Throwable {
+		PScoutReader.readAPIMappings(this.getClass().getResourceAsStream(
+				"/data/gingerbread_allmappings"));
 	}
 }
