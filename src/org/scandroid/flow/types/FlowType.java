f756c8989410445bc4413afbc8778b5e63636a86
Aaron Tomb <atomb@galois.com>
2012-11-08 10:53:29 -0800

    Use only block number in FlowType hashCode.

--- src/org/scandroid/flow/types/FlowType.java
+++ src/org/scandroid/flow/types/FlowType.java
@@ -82,7 +82,7 @@ public abstract class FlowType<E extends ISSABasicBlock> {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((block == null) ? 0 : block.hashCode());
+        result = prime * result + ((block == null) ? 0 : block.getNumber());
         result = prime * result + (source ? 1231 : 1237);
         return result;
     }
@@ -100,7 +100,7 @@ public abstract class FlowType<E extends ISSABasicBlock> {
         if (block == null) {
             if (other.block != null)
                 return false;
-        } else if (!compareBlocks(block, other.block)) {
+        } else if (block.getNumber() != other.block.getNumber()) {
             return false;
         }
         if (source != other.source)
